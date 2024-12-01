eaf6ab6705954b0683e8616cff0b41b500c5b536
KYSF-24 <kheder.ysf@hotmail.com>
2024-12-01 14:35:22 +0300

    Hill Climbing

--- main.py
+++ main.py
@@ -2,18 +2,30 @@ from boardloader import BoardLoader
 from game import Game
 from solver import Solver
 from state import State
-file = open('Levels/level_21.txt')
+
+file = open('Levels/level_9.txt')
 board = BoardLoader.transform_board_from_file_to_2d_list(file)
 game = Game(board)
 s0 = State(game)
 solver = Solver(s0)
-print("UCS : ")
-for state in solver.ucs()[0]:
+
+print("Hill Climbing : ")
+for state in solver.hill_climbing():
     print(state.game)
-print("UCS Visited List : ")
-for state in solver.ucs()[1]:
-    print(state.game)
-print("UCS Cost : " , solver.ucs()[2])
+
+# print("A* : ")
+# for state in solver.astar()[0]:
+#     print(state.game)
+# print("A* Visited List : ",solver.astar()[1])
+# print("A* Cost : " , solver.astar()[2])
+
+# print("UCS : ")
+# for state in solver.ucs()[0]:
+#     print(state.game)
+# print("UCS Visited List : ")
+# for state in solver.ucs()[1]:
+#     print(state.game)
+# print("UCS Cost : " , solver.ucs()[2])
 # print("Recursive DFS : ")
 # for state in solver.recursive_dfs(s0)[0]:
 #     print(state.game)
